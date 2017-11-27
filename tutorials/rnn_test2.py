0b1f7c2254ef956046bbfd45e1bdd56aa65ec427
Nitish Joshi <joshinh@gmail.com>
2017-11-27 12:22:28 +0530

    LSTM Tensorflow API trial

---
+++ tutorials/rnn_test2.py
@@ -0,0 +1,88 @@
+import numpy as np 
+import tensorflow as tf
+
+num_epochs = 20
+total_series_length = 50000
+truncated_backprop_length = 15
+state_size = 4
+num_classes = 2
+echo_step = 3
+batch_size = 5
+num_batches = total_series_length//batch_size//truncated_backprop_length
+
+def generateData():
+    x = np.array(np.random.choice(2, total_series_length, p=[0.5, 0.5]))
+    y = np.roll(x, echo_step)
+    y[0:echo_step] = 0
+
+    x = x.reshape((batch_size, -1))  # The first index changing slowest, subseries as rows
+    y = y.reshape((batch_size, -1))
+
+    return (x, y)
+
+   
+batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
+batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
+
+cell_state = tf.placeholder(tf.float32, [batch_size, state_size])
+hidden_state = tf.placeholder(tf.float32, [batch_size, state_size])
+init_state = tf.contrib.rnn.LSTMStateTuple(cell_state, hidden_state)
+
+W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)
+b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)
+
+# Unpack columns
+inputs_series = tf.split(batchX_placeholder,truncated_backprop_length, 1)
+labels_series = tf.unstack(batchY_placeholder, axis=1)
+
+# Forward passes
+cell = tf.contrib.rnn.BasicLSTMCell(state_size, state_is_tuple=True)
+states_series, current_state = tf.contrib.rnn.static_rnn(cell, inputs_series, init_state)
+
+logits_series = [tf.matmul(state, W2) + b2 for state in states_series] #Broadcasted addition
+predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
+
+losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=_logits, labels=_labels) for _logits, _labels in zip(logits_series,labels_series)]
+total_loss = tf.reduce_mean(losses)
+
+train_step = tf.train.AdagradOptimizer(0.1).minimize(total_loss)
+
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    # plt.ion()
+    # plt.figure()
+    # plt.show()
+    loss_list = []
+    # x,y = generateData()
+
+    for epoch_idx in range(num_epochs):
+        x,y = generateData()
+        _current_cell_state = np.zeros((batch_size, state_size))
+        _current_hidden_state = np.zeros((batch_size, state_size))
+
+        print("New data, epoch", epoch_idx)
+
+        for batch_idx in range(num_batches):
+            start_idx = batch_idx * truncated_backprop_length
+            end_idx = start_idx + truncated_backprop_length
+
+            batchX = x[:,start_idx:end_idx]
+            batchY = y[:,start_idx:end_idx]
+
+            _total_loss, _train_step, _current_state, _predictions_series = sess.run(
+                [total_loss, train_step, current_state, predictions_series],
+                feed_dict={
+                    batchX_placeholder:batchX,
+                    batchY_placeholder:batchY,
+                    cell_state: _current_cell_state,
+                    hidden_state: _current_hidden_state
+
+                })
+
+            _current_cell_state, _current_hidden_state = _current_state
+
+            loss_list.append(_total_loss)
+
+            if batch_idx%100 == 0:
+                print("Step",batch_idx, "Loss", _total_loss)
+                # plot(loss_list, _predictions_series, batchX, batchY)
